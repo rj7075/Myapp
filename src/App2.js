e2a9ed1dc46e62689529c538bc2804191e1b1f0e
rj7075 <rj7075yadav.com>
2024-08-16 00:12:40 +0530

    first commit

---
+++ src/App2.js
@@ -0,0 +1,11 @@
+function App2(){
+    return(
+        <>
+        <div>
+            <h1>Hi </h1>
+            
+        </div>
+        </>
+    )
+}
+export default App2;
---
+++ src/ranjeet.js
@@ -0,0 +1,11 @@
+import App2 from './App2';
+function App1 (){
+    return(
+        <>
+        <h1>Welcome to Rajeshwers ERA</h1>
+         <App2/>
+         </>
+
+    )
+}
+export default App1;
